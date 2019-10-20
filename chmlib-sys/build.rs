fdb920c50bf012091445cd8581ef1c8f2251eb3e
Michael Bryan <michaelfbryan@gmail.com>
2019-10-20 19:55:08 +0800

    Moved the vendor directory inside chmlib-sys so it can be published

--- chmlib-sys/build.rs
+++ chmlib-sys/build.rs
@@ -5,8 +5,7 @@ fn main() {
     let project_dir = PathBuf::from(env::var("CARGO_MANIFEST_DIR").unwrap())
         .canonicalize()
         .unwrap();
-    let root_dir = project_dir.parent().unwrap();
-    let src = root_dir.join("vendor").join("CHMLib").join("src");
+    let src = project_dir.join("vendor").join("CHMLib").join("src");
 
     Build::new()
         .file(src.join("chm_lib.c"))
